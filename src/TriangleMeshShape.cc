05071950ba83eec15a28a4af38c1e9e066ffcf52
Michael Morrison <mmorrison@wootalyzer.com>
2013-05-29 07:01:54 -0500

    bump

--- src/TriangleMeshShape.cc
+++ src/TriangleMeshShape.cc
@@ -4,14 +4,17 @@ OBJECT_INIT_START(TriangleMeshShape)
 OBJECT_INIT_END()
 
 OBJECT_NEW_START(TriangleMeshShape)
-	Local<Array> verts = args[0]->ToArray();
+	if(!args[0]->IsArray()) return ThrowException(String::New("Invalid Argument"));
+	if(!args[1]->IsArray()) return ThrowException(String::New("Invalid Argument"));
+
+	Local<Array> verts = Local<Array>(Array::Cast(*args[0]));
 	int vertCount = verts->Length()/3;
-	Local<Array> tris = args[1]->ToArray();
+	Local<Array> tris = Local<Array>(Array::Cast(*args[1]));
 	int triCount = tris->Length()/3;
 
 	self->vertices = new btVector3[vertCount];
 	for(int i = 0; i < vertCount; i++) {
-		vertices[i].setValue(
+		self->vertices[i].setValue(
 			verts->Get(Integer::New(i*3))->ToNumber()->Value(),
 			verts->Get(Integer::New(i*3+1))->ToNumber()->Value(),
 			verts->Get(Integer::New(i*3+2))->ToNumber()->Value()
@@ -19,9 +22,9 @@ OBJECT_NEW_START(TriangleMeshShape)
 	}
 	self->triangles = new int[triCount*3];
 	for(int i = 0; i < triCount; i++) {
-		triangles[i*3] = tris->Get(Integer::New(i*3))->ToNumber()->Value();
-		triangles[i*3+1] = tris->Get(Integer::New(i*3+1))->ToNumber()->Value();
-		triangles[i*3+2] = tris->Get(Integer::New(i*3+2))->ToNumber()->Value();
+		self->triangles[i*3] = tris->Get(Integer::New(i*3))->ToNumber()->Value();
+		self->triangles[i*3+1] = tris->Get(Integer::New(i*3+1))->ToNumber()->Value();
+		self->triangles[i*3+2] = tris->Get(Integer::New(i*3+2))->ToNumber()->Value();
 	}
 
 	self->vertexArray = new btTriangleIndexVertexArray(
@@ -32,7 +35,7 @@ OBJECT_NEW_START(TriangleMeshShape)
 	self->shape = new btBvhTriangleMeshShape(self->vertexArray,true);
 OBJECT_NEW_END()
 
-OBJECT_DELETE_START(ConvexHullShape)
+OBJECT_DELETE_START(TriangleMeshShape)
 	delete shape;
 	delete vertexArray;
 	delete[] triangles;
