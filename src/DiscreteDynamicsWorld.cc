aa8d4549c4ee9a4a2cbcfb35bde65d78f798c1b0
Michael Morrison <mmorrison@wootalyzer.com>
2013-06-13 00:18:27 -0500

    bump

--- src/DiscreteDynamicsWorld.cc
+++ src/DiscreteDynamicsWorld.cc
@@ -1,5 +1,5 @@
 #include "DiscreteDynamicsWorld.h"
-
+#include "BulletCollision/CollisionDispatch/btInternalEdgeUtility.h"
 
 extern ContactAddedCallback gContactAddedCallback;
 static bool CustomMaterialCombinerCallback(btManifoldPoint& cp,	const btCollisionObjectWrapper* colObj0Wrap,int partId0,int index0,const btCollisionObjectWrapper* colObj1Wrap,int partId1,int index1)
